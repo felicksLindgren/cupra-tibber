f9bc82f44d6dd37934ec88e18c5f506e8ee5c71e
Felix Lindgren <felix.lindgren@omegapoint.se>
2025-06-02 20:59:16 +0200

    Refactor access token retrieval in main.sh to streamline response handling

--- main.sh
+++ main.sh
@@ -3,12 +3,12 @@ CLIENT_SECRET=$CLIENT_SECRET
 TOKEN_ENDPOINT=$TOKEN_ENDPOINT
 REFRESH_TOKEN=$REFRESH_TOKEN
 
-response=$(curl -s -X POST "$ACCESS_TOKEN_URL" \
+curl -s -X POST "$ACCESS_TOKEN_URL" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "grant_type=refresh_token" \
   -d "client_id=$CLIENT_ID" \
   -d "client_secret=$CLIENT_SECRET" \
-  -d "refresh_token=$REFRESH_TOKEN")
+  -d "refresh_token=$REFRESH_TOKEN" | jq
 
-echo $response | jq
+
 
