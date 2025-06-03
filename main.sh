e338e5e869050a386111a0f2d1d43b572fa75741
Felix Lindgren <felix.lindgren@omegapoint.se>
2025-06-03 20:55:50 +0200

    Improve GitHub Secrets update by adding missing URL echo and simplifying JSON payload formatting

--- main.sh
+++ main.sh
@@ -74,15 +74,14 @@ else
   echo "Refresh token encrypted successfully."
 fi
 
+echo "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/secrets/REFRESH_TOKEN"
+
 put_response=$(curl -s -L -X PUT \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization Bearer $GH_PAT" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/secrets/REFRESH_TOKEN" \
-  -d "{
-    \"encrypted_value\": \"$encrypted_value\",
-    \"key_id\": \"$key_id\"
-  }")
+  -d "{ \"encrypted_value\": \"$encrypted_value\", \"key_id\": \"$key_id\" }")
 
 # Check for "201 Created" status code
 if [ "$(echo "$put_response" | jq -r '.status')" != "201" ]; then
