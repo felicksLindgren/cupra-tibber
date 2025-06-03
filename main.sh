348f5258bc2bfda83676dff73f3f7a6b00088a6f
Felix Lindgren <felix.lindgren@omegapoint.se>
2025-06-03 21:03:46 +0200

    Fix authorization header format in GitHub Secrets update request

--- main.sh
+++ main.sh
@@ -78,7 +78,7 @@ echo "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/secrets/REFRES
 
 put_response=$(curl -s -L -X PUT \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization Bearer $GH_PAT" \
+  -H "Authorization: Bearer $GH_PAT" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/secrets/REFRESH_TOKEN" \
   -d "{ \"encrypted_value\": \"$encrypted_value\", \"key_id\": \"$key_id\" }")
